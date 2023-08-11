28c86da525ac288ceae5cea40da1d06e460483f0
deependrav-talentica <deependra.verma@talentica.com>
2023-08-11 15:56:26 +0530

    initial commit

---
+++ ConsumerServiceApplication/src/main/java/com/stickyio/consumerserviceapplication/ConsumerServiceApplication.java
@@ -0,0 +1,10 @@
+package com.stickyio.consumerserviceapplication;
+
+import org.springframework.boot.SpringApplication;
+
+public class ConsumerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerServiceApplication.class, args);
+    }
+}
---
+++ TrackingServiceApplication/src/main/java/com/stickyio/trackingserviceapplication/TrackingServiceApplication.java
@@ -0,0 +1,14 @@
+/* * Copyright 2023-2024 the original author or authors. * * TBD */
+package com.stickyio.trackingserviceapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrackingServiceApplication.class, args);
+    }
+
+}
